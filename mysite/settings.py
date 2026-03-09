0cfc949456c2ea710c911a46c9b738efc78989cf
Riiion <l88909352@gmail.com>
2026-03-09 22:58:30 +0900

    fix allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-idjk$=i4b_r4pm&y@2_6^enj&*avp5st&+cb+jy%diwhcnux5&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = ["kosodate-app.onrender.com", "localhost", "127.0.0.1"]
 
